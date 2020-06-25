e39bcb6db6a1154e37a02b4bf54662da9ffdd5e6
luis guzman <antilou86@gmail.com>
2020-06-25 17:54:47 -0400

    most of the logic is complete. now bughunting, cells wont render on each generation even though the arrays are correctly updating.

--- src/Game.js
+++ src/Game.js
@@ -8,35 +8,17 @@ const height = 600;
 const rows = height/cellSize;
 const cols = width/cellSize;
 
-
-//the actual functional component, ayyyyy
 const Game = () => {
     const [cells, setCells] = useState([]);
     const [board, setBoard] = useState([]);
     const [lastTouched, setLastTouch] = useState([]);
     const [isRunning, setIsRunning] = useState(false);
     const [interval, setInterval] = useState(100);
-
-    const runIteration = () => {
-        console.log('running iteration')
-    }
-    const startGame = () => {
-        setIsRunning(true);
-        runIteration();
-    }
-
-    const stopGame = () => {
-        setIsRunning(false)
-        if (timeoutHandler) {
-            window.clearTimeout(timeoutHandler);
-            timeoutHandler=null;
-        }
-    }
-    
+    const [timeoutHandler, setTimeoutHandler] = useState()
     const handleIntervalChange= (e) => {
         setInterval(e.target.value)
     }
-
+    
     const makeEmptyBoard = () => {
         let board = []
         //creates empty arrays equal to number of rows.
@@ -47,13 +29,75 @@ const Game = () => {
                 board[y][x] = false;
             }
         }
-        console.log("what makeEmptyBoard() returns", board)
-        setBoard(board)
+        return board
+    }
+    
+    useEffect(()=> {
+        setBoard(makeEmptyBoard())
+    },[])
+
+    const calculateNeighbors = (x,y,board) => {
+        //returns number of living neighbors
+        let count = 0;
+
+        //offset directions for all eight possible neighbors
+        const directions = [[-1,-1], [-1,0], [-1,1], [0,1], [1,1], [1,0], [1,-1], [0,-1]]
+
+        //loops through all eight neighbor positions
+        for(let i=0; i<directions.length; i++){
+            const dir = directions[i]
+            let y1 = y + dir[0];
+            let x1 = x + dir[1];
+
+            //if board[x1][y1] is within the bounds of the grid, and is set to true
+            if(x1 >= 0 && x1 < cols && y1 >= 0 && y1 < rows && board[y1][x1]) {
+                count++
+            }
+        }
+        return count
     }
 
-    useEffect(()=> {
-        makeEmptyBoard()
-    },[])
+    console.log("board before runIteration()", board)
+    const runIteration = () => {
+        console.log('running iteration');
+        console.log("board inside run iteration", board)
+        let newBoard = makeEmptyBoard();
+        //loop through board
+        for(let y=0; y<rows; y++) {
+            for(let x=0; x<cols; x++){
+
+                let neighbors = calculateNeighbors(x,y,board)
+
+                //if its a living cell and has two or three neighbors it stays the same
+                if(board[y][x] && (neighbors===2 || neighbors===3)){
+                    newBoard[y][x] = true
+                }
+                //if a dead cell has three neighbors it resurrects
+                else if(!board[y][x] && neighbors===3){
+                    newBoard[y][x] = true
+                }
+                //all other cells die or stay dead
+                else {
+                    newBoard[y][x] = false
+                }
+            }
+        }
+        setBoard(newBoard)
+        setCells(makeCells())
+        setTimeoutHandler(window.setTimeout(()=>{runIteration()}, interval))
+    }
+    const startGame = () => {
+        setIsRunning(true);
+        runIteration();
+    }
+
+    const stopGame = () => {
+        setIsRunning(false)
+        if (timeoutHandler) {
+            window.clearTimeout(timeoutHandler);
+            setTimeoutHandler(null);
+        }
+    }
 
     //helper function to pull x,y coordinates off of the window
     const getElementOffset = (e) => {
@@ -97,15 +141,18 @@ const Game = () => {
         console.log("lastTouched2=",lastTouched)
 
         if(x===0 && y===0 && (lastTouched[lastTouched.length-1].x !== 0 || lastTouched[lastTouched.length-1].y !==0)) {
-            console.log(cells[cells.length-1])
-            console.log("before changes",lastTouched)
-            setBoard(prevBoard => ([...prevBoard, board[lastTouched[lastTouched.length-1].y][lastTouched[lastTouched.length-1].x] = !board[lastTouched[lastTouched.length-1].y][lastTouched[lastTouched.length-1].x]]))
+            let updateBoard = [...board]
+            const lastY = lastTouched[lastTouched.length-1].y
+            const lastX = lastTouched[lastTouched.length-1].x
+            updateBoard[lastY][lastX]=!updateBoard[lastY][lastX]
+            setBoard(updateBoard)            
             setLastTouch(prevTouched => (prevTouched.slice(0,prevTouched.length-2)))
-            console.log("before changes",lastTouched)
         }
 
         else if(x >= 0 && x <= cols && y >= 0 && y <= rows) {
-            setBoard(prevBoard => ([...prevBoard, board[y][x] = !board[y][x]]))
+            let updateBoard = [...board]
+            updateBoard[y][x] =!updateBoard[y][x]
+            setBoard(updateBoard)
         }
 
     }
